cf7eff6735f945504fa24128256d7b5d23779bd2
Mathis Veber Linux <orvaultbis@gmail.com>
2024-01-22 10:40:40 +0100

    mission 2 complete

--- src/robot_app/copilot.c
+++ src/robot_app/copilot.c
@@ -5,74 +5,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define PATH_STEPS	6
-#define AVOIDANCE_STEPS 7
 
 static move_status path_status = IDLE;
-static move path[PATH_STEPS]=
-{
-	{ROTATION,{U_TURN},20},
-	{FORWARD,{1},20},
-	{ROTATION,{LEFT},20},
-	{FORWARD,{.distance=1},20},
-	{ROTATION,{RIGHT},50},
-	{FORWARD,{10},80}
-	
-};
 
-
-static move avoidance_path[AVOIDANCE_STEPS] =
-{
-    {ROTATION, {RIGHT}, 15},
-    {FORWARD, {2}, 15},
-    {ROTATION, {LEFT}, 15},
-    {FORWARD, {2}, 15},
-    {ROTATION, {LEFT}, 15},
-    {FORWARD, {2}, 15},
-    {ROTATION, {RIGHT}, 15}
-};
-
+static move *path = NULL;
 static int current_step = 0;
+static int path_steps = 0;
+
 
 extern void copilot_start_path(void) {
-	/*TODO: Start the path with move path[0] */
-	/*HINT: A path can only start if a path is not already in progress */
-	if (pilot_get_status() != IN_PROGRESS){
-		current_step = 0;
-		pilot_start_move(path[current_step]);
-	}
+  if ((path_status == IDLE) && (path_steps > 0)) {
+    path_status = IN_PROGRESS;
+    pilot_start_move(path[0]);
+  }
 }
 
 extern void copilot_next_step(void)
 {
-	/*TODO: Start next move if it exists, otherwise the path is DONE */
-	/*HINT: Update current_step index */
-
-	if (current_step < PATH_STEPS){
-		current_step+=1;
+	current_step++;
+	if( current_step >= path_steps )
+	{
+		path_status = DONE;
+	}
+	else
+	{
 		pilot_start_move(path[current_step]);
 	}
-	else{
-		path_status = DONE;
-	}	
 }
 
 extern move_status copilot_stop_at_step_completion(void)
 {
-	/*TODO: if pilot status is !DONE and target is reached, then go to next step */
-	/*HINT: It might be useful for the future to reset path_status here to IN_PROGRESS */
-	
-	/*Special management of obstacle detection, no effect yet */
-	if( pilot_get_status()==OBSTACLE && path_status != OBSTACLE)
+	if( pilot_get_status()!=DONE && pilot_stop_at_target()==DONE )
+	{
+		path_status = IN_PROGRESS;
+		copilot_next_step();
+	}
+	else if( pilot_get_status()==OBSTACLE && path_status != OBSTACLE )
 	{
 		path_status = OBSTACLE;
-		copilot_insert_avoidance();
-		printf("ici obstacle \n");
-	}
-	else if (pilot_get_status()!= DONE && pilot_stop_at_target() == DONE){
 		copilot_next_step();
-		path_status=IN_PROGRESS;
-
 	}
 	return pilot_get_status();
 }
@@ -82,31 +53,40 @@ extern int copilot_is_path_completed(void) {
 }
 
 
+extern int copilot_create_path(int steps_nb) {
+  if ((path == NULL) && (steps_nb > 0)) // dont'allocate if path already existing
+  {
+    /*TODO: allocate path, */
+    /* why calloc is better than malloc ? */
+    if (path != NULL) {
+	/*TODO: Update path_steps */
+      return EXIT_SUCCESS;
+    }
+  }
+  return EXIT_FAILURE;
+}
 
+extern int copilot_destroy_path(void) {
+  if (path != NULL) {
+	/*TODO: free memory */
+    path = NULL;
+	/*TODO: update path_steps */
+    TRACE("Copilot destroyed the current path.\n");
+    return EXIT_SUCCESS;
+  }
+  return EXIT_FAILURE;
+}
 
-extern void copilot_insert_avoidance(void)
-{
-	/*TODO: Allocate a temp_path to save the current path before deletion (error management if allocation fails) */
-	/*Copy the current path steps to the temp_path */
-	/*Destroy the path and create a new one with its previous size plus AVOIDANCE_STEPS */
-	/*3 intelligent for loops with complementary indexes are enough to copy the previous path steps up to the current step (when obstacle has been detected) + the avoidance steps + the remaining path steps */
-	
+extern int copilot_add_step(int index, move step) {
+	/*TODO: check index validity before adding step */
+    return EXIT_SUCCESS;
+  return EXIT_FAILURE;
+}
 
-	//int temp_path=copilot_get_number_steps_in_path(); 
-	//copilot_stop_at_step_completion()
-	printf("debut evitement \n");
-	int avoidance_step=0;
-	do{
-		pilot_start_move(avoidance_path[avoidance_step]);
-		if(pilot_get_status()==OBSTACLE && path_status != OBSTACLE){
-			copilot_insert_avoidance();
-		}
-		avoidance_step+=1;
-	}while( avoidance_step < AVOIDANCE_STEPS);	
-	printf("fin evitement \n");	
-	}
+move copilot_get_step(int index) {
+	move step = {0};
+	/*TODO: check index validity before returning step */
+  	return step;
+}
 
-
-
-
-
+extern int copilot_get_number_steps_in_path() { return path_steps; }
--- src/robot_app/copilot.h
+++ src/robot_app/copilot.h
@@ -28,5 +28,53 @@ extern void copilot_next_step(void);
  */
 extern move_status copilot_stop_at_step_completion(void);
 
-extern void copilot_insert_avoidance(void);
+/**
+ * @brief Checks if the entire path is completed.
+ *
+ * @return True if the path is completed, otherwise false.
+ */
+extern int copilot_is_path_completed(void);
+
+/**
+ * @brief Destroys the current path.
+ * @return EXIT_SUCCESS if the path is destroyed, otherwise EXIT_FAILURE.
+ */
+extern int copilot_destroy_path(void);
+
+/**
+ * @brief Adds a step to the path at a specific index.
+ *
+ * @param index The index at which to add the step.
+ * @param type The type of step to be added to the path.
+ * @param value the distance or degree of the move
+ * @return EXIT_SUCCESS if the step is added successfully, otherwise
+ * EXIT_FAILURE.
+ */
+extern int copilot_add_step(int index, move step);
+
+/**
+ * @brief get the step at a specific index.
+ *
+ * @param index The index at which to get the step
+ * @return return the move of the indexed step or Zero move if index exceed path
+ * limits
+ */
+extern move copilot_get_step(int index);
+
+/**
+ * @brief allocate memory for a new path.
+ *
+ * @param steps_nb number of steps of the new path
+ * @return return EXIT_SUCCESS if the path is created successfully, otherwise
+ * EXIT_FAILURE.
+ */
+extern int copilot_create_path(int steps_nb);
+
+/**
+ * @brief get the number of steps of the current path.
+ *
+ * @return return the number of steps of the path
+ */
+extern int copilot_get_number_steps_in_path();
+
 #endif // COPILOT_H
